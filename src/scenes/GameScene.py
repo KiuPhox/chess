cf8c25501f12cde05d634bf564dbef43801281c2
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2024-05-04 22:27:13 +0700

    Remove background object to improve performance

--- src/scenes/GameScene.py
+++ src/scenes/GameScene.py
@@ -1,12 +1,5 @@
-from constants.AssetPath import ImagePath
-
-from engine.GameObject import GameObject
-from engine.components.Sprite import Sprite
-
 from objects.Board import Board
 from scenes.Scene import Scene
-
-from objects.Background import Background
 
 
 class GameScene(Scene):
@@ -14,11 +7,7 @@ class GameScene(Scene):
         super().__init__("GameScene", screen)
 
     def start(self):
-        self.create_background()
         self.create_board()
-
-    def create_background(self):
-        self.bg = Background(self)
 
     def create_board(self):
         self.board = Board(self)
--- src/scenes/MenuScene.py
+++ src/scenes/MenuScene.py
@@ -5,15 +5,10 @@ from engine.components.Sprite import Sprite
 
 from scenes.Scene import Scene
 
-from objects.Background import Background
-
 
 class MenuScene(Scene):
     def __init__(self, screen):
         super().__init__("MenuScene", screen)
 
     def start(self):
-        self.create_background()
-
-    def create_background(self):
-        self.bg = Background(self)
+        pass
